5b04307d6a5f943605ea8ade0822bd35f214e431
Vasili Lukinykh <bazil.lucky@gmail.com>
2023-10-07 21:10:58 -0400

    links with information added

--- kafka-messaging/src/main/java/com/example/kafkamessaging/KafkaController.java
+++ kafka-messaging/src/main/java/com/example/kafkamessaging/KafkaController.java
@@ -1,26 +1,25 @@
 package com.example.kafkamessaging;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+// following articles used
+// https://www.stackchief.com/blog/%40KafkaListener%20%7C%20Spring%20Boot%20Example
+// https://www.baeldung.com/spring-kafka
+// https://coderanch.com/t/770966/java/Kafka-consumer-message-consumed-multiple
+// https://levelup.gitconnected.com/how-to-integrate-kafka-with-spring-boot-47c0ee01b65d
+//
 @RestController
 public class KafkaController {
     private final KafkaProducerService producerService;
-    private final KafkaConsumerService consumerService;
 
-    public KafkaController(KafkaProducerService producerService, KafkaConsumerService consumerService) {
+    public KafkaController(KafkaProducerService producerService) {
         this.producerService = producerService;
-        this.consumerService = consumerService;
     }
 
     @PostMapping("/messages")
     public void sendMessageToKafka(@RequestBody String message) {
         producerService.sendMessage(message);
     }
-    @GetMapping("/messages")
-    public String getMessagesFromKafka(){
-        return "Hello";
-    }
+
 }
